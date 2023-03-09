447db7c6ae5ff1ae3569c082e8fa8211b5267cdc
npeyrouset <nils.peyrouset@ensae.fr>
2023-03-09 10:37:19 +0000

    add change graph from file dist=1

--- delivery_network/graph.py
+++ delivery_network/graph.py
@@ -180,7 +180,7 @@ def graph_from_file(filename):
             edge = list(map(int, file.readline().split()))
             if len(edge) == 3:
                 node1, node2, power_min = edge
-                g.add_edge(node1, node2, power_min) # will add dist=1 by default
+                g.add_edge(node1, node2, power_min, dist=1) # will add dist=1 by default
             elif len(edge) == 4:
                 node1, node2, power_min, dist = edge
                 g.add_edge(node1, node2, power_min, dist)
