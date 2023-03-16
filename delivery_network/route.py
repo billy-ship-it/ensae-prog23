ef2075cf9cae421cf9ee80de42899c4b007178b8
npeyrouset <nils.peyrouset@ensae.fr>
2023-03-16 10:28:10 +0000

    add file route

---
+++ delivery_network/route.py
@@ -0,0 +1,18 @@
+
+
+def route_from_file(filename):
+    """ La fonction prend un fichier routes.nombre.in en entrée et crée 
+    un fichier routes.nombre.out dans le sous-dossier output dans lequel
+    le numéro sur chaque ligne correspond à la puissance minimale pour 
+    couvrir le trajet
+    """
+    with open(filename, "r") as file:
+        liste = list(file)
+        for k in range(len(liste)):
+            liste[k] = liste[k].split()
+    
+    with open(filename.replace("in", "out"), 'w') as file2:
+        for k in range((len(liste))):
+            if len(liste[k]) == 3:
+                src, dest, utilite = liste[k]
+                file2.write(str(graph_from_file(filename.replace("routes", "network")).min_power(int(src), int(dest))[1]) + "\n")
