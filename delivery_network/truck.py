753ee5e58c44b621b239ca049f849dedef24dc32
npeyrouset <nils.peyrouset@ensae.fr>
2023-03-16 13:14:13 +0000

    add function budget_trajets

--- delivery_network/truck.py
+++ delivery_network/truck.py
@@ -33,14 +33,15 @@ class Trucks:
         return dict(sorted(self.puissance.items(), key=lambda item :item[1]))
 
     def camion_trie_cout(self):
-         """Cette fonction renvoie un dictionnaire trié suivant 
+        """Cette fonction renvoie un dictionnaire trié suivant 
         le coût croissant des camions du catalogue
         """
         return dict(sorted(self.cout.items(), key=lambda item : item[1]))
+        
 
-# calculer le budget pour couvrir tous les trajets
-
-
+#calculer le budget pour effectuer tous les trajets
+#calculer pour un trajet donnée avec une certaine utilité, le camion le moins cher qui a la puissance suffisante pour
+#pouvoir faire le trajet
 
 def truck_from_file(filename):
     """ créer un dictionnaire avec pour clés les catégories de camions et en
@@ -61,3 +62,23 @@ def truck_from_file(filename):
             t.puissance[k] = (lines[k][0])
             t.cout[k] = (lines[k][1])
         return t
+
+def budget_trajets(filename_trucks):
+    """ Cette fonction calcule le budget qui serait nécéssaire pour couvrir
+    l'ensemble des trajets
+    """
+    budget = 0
+    t = truck_from_file(filename_trucks)  # ici on a accès à un graphe
+    puissance_trie = t.camion_trie_puissance()        
+    with open(filename_trucks.replace("trucks", "routes").replace("in", "out"), 'r') as file:
+        puissance = file.readlines()
+        for k in range(1, len(puissance)):
+            puissance[k] = puissance[k].split()
+            puissance[k] = int(puissance[k][0])
+    for k in range(1, len(puissance)):
+        for camion in list(puissance_trie.keys()):
+            if puissance_trie[camion] >= puissance[k]:
+                budget += t.cout[camion]
+                break
+    return budget
+
