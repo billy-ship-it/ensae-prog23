116b79f1e70cd355364503dfd119794ff51c6fae
npeyrouset <nils.peyrouset@ensae.fr>
2023-03-30 12:04:56 +0000

    add changes : test everything

--- tests/test_s1q6_minimal_power3.py
+++ tests/test_s1q6_minimal_power3.py
@@ -11,13 +11,13 @@ class Test_MinimalPower(unittest.TestCase):
     def test_network0(self):
         g = graph_from_file("input/network.00.in")
         arbre, hauteur, puissance = g.dictionnaire_kruskal()
-        self.assertEqual(g.min_power3(1, 4, arbre, hauteur, puissance), g.min_power(1, 4)[0])
-        self.assertEqual(g.min_power3(2, 4, arbre, hauteur, puissance), g.min_power(2, 4)[0])
+        self.assertEqual(g.min_power3(1, 4, arbre, hauteur, puissance), g.min_power(1, 4))
+        self.assertEqual(g.min_power3(2, 4, arbre, hauteur, puissance), g.min_power(2, 4))
 
     def test_network1(self):
         g = graph_from_file("input/network.04.in")
         arbre, hauteur, puissance = g.dictionnaire_kruskal()
-        self.assertEqual(g.min_power3(1, 4, arbre, hauteur, puissance), g.min_power(1, 4)[0])
+        self.assertEqual(g.min_power3(1, 4, arbre, hauteur, puissance), g.min_power(1, 4))
 
 if __name__ == '__main__':
     unittest.main()
