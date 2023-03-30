d11763e5ffa586a62712ef0846e392d95f1929ca
npeyrouset <nils.peyrouset@ensae.fr>
2023-03-30 07:58:43 +0000

    add test min power 3

---
+++ tests/test_s1q6_minimal_power3.py
@@ -0,0 +1,22 @@
+# This will work if ran from the root folder.
+import sys 
+sys.path.append("delivery_network")
+
+from graph import graph_from_file
+import unittest   # The test framework
+from graph import *
+
+class Test_MinimalPower(unittest.TestCase):
+    def test_network0(self):
+        g = graph_from_file("input/network.00.in")
+        arbre, hauteur, puissance = g.dictionnaire_kruskal()
+        self.assertEqual(g.min_power3(1, 4, arbre, hauteur, puissance), g.min_power(1, 4)[0])
+        self.assertEqual(g.min_power3(2, 4, arbre, hauteur, puissance), g.min_power(2, 4)[0])
+
+    def test_network1(self):
+        g = graph_from_file("input/network.04.in")
+        arbre, hauteur, puissance = g.dictionnaire_kruskal()
+        self.assertEqual(g.min_power3(1, 4, arbre, hauteur, puissance), g.min_power(1, 4)[0])
+
+if __name__ == '__main__':
+    unittest.main()
