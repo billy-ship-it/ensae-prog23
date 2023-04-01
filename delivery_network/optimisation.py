d1c93456ea826853ae7d4395a992e40c5318322d
npeyrouset <nils.peyrouset@ensae.fr>
2023-04-01 14:50:41 +0000

    add sorted rapport file

--- delivery_network/optimisation.py
+++ delivery_network/optimisation.py
@@ -4,6 +4,26 @@ from truck import *
 from route import *
 
 
+
+def sort(filename_routeXtrucks):
+    """Cette fonction trie les lignes des fichiers routestrucks
+    suivant le rapport de l'utilité décroissant
+    """
+    with open(filename_routeXtrucks, 'r') as f:
+        lines = f.readlines()
+        for k in range(len(lines)):
+            lines[k] = lines[k].split()
+            for j in range(0, 2):
+                lines[k][j] = int(lines[k][j])
+            lines[k][2] = float(lines[k][2])
+    lines.sort(key=lambda item: item[2], reverse=True)
+
+    with open( "/home/onyxia/work/ensae-prog23/output/sorted" + str(filename_routeXtrucks[filename_routeXtrucks.find("routes"):]), 'w') as file:
+        for k in range(len(lines)):
+            file.write(str(lines[k][0]) + " " + str(lines[k][1]) + " " + str(lines[k][2]) + "\n")
+    
+
+
 def optimisation_profit(filename_routes, filename_trucks, budget):
     routes = route_from_file(filename_routes)
     camions = truck_from_file(filename_trucks)
@@ -41,5 +61,38 @@ def optimisation_profit(filename_routes, filename_trucks, budget):
 
     return (max_value, selected_item)
 
+def calcul_profit(B, filename_sortedroutesXtrucks):
+    t = truck_from_file("/home/onyxia/work/ensae-prog23/input" + filename_sortedroutesXtrucks[filename_sortedroutesXtrucks.find(trucks):].replace("out", "in"))
+    depense = 0
+    profit = 0
+    k = 0
 
-def knapsack(B, filename_trucks, filename)
+    cout_camion_min = t.cout[t.camion_cout_min()]
+
+    with open(filename_sortedroutesXtrucks, 'r') as f:
+        lines = f.readlines()
+        for k in range(len(lines)):
+            lines[k] = lines[k].split()
+            for j in range(0, 2):
+                lines[k][j] = int(lines[k][j])
+    
+    while depense < B:
+        cout, utilite = lines[k][0:1]
+        profit += utilite
+        depense += cout
+        k += 1
+
+    marge = B - depense
+
+    if marge < cout_camion_min:
+        return profit, depense
+    else:
+        while marge >= cout_camion_min:
+            cout, utilite = lines[k][0:1]
+            profit += utilite
+            depense
+
+
+
+
+
